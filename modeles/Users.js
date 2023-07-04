5feeccefff015995240b0ec66ba29345d475b77f
Alain Pilon <Alain.Pilon@clg.qc.ca>
2023-07-04 16:30:37 -0400

    fin du laboratoire 3 - ajout des API pour les usagers et tests

--- modeles/Users.js
+++ modeles/Users.js
@@ -34,3 +34,53 @@ let Users = module.exports = mongoose.model('usagers', schemaUsagers);
 module.exports.getUsers = (callback, limit) => {
     Users.find(callback).limit(limit);
 }
+
+// fonction pour obtenir tous les usagers de la collection qui correspondent à l'_id
+// reçoit en paramètre un identifiant, une fonction callback et le nombre maximum (limit) de usagers à retourner
+module.exports.getUsersParId = (_id, callback, limit) => {
+    let filtre = {"_id": _id};
+    Users.find(filtre, callback).limit(limit);
+}
+
+// fonction pour obtenir tous les usagers de la collection qui contiennent une partie d'un champ
+// reçoit en paramètre le champ, les caractères à chercher dans le champ, une fonction callback et le nombre maximum (limit) de livres à retourner
+module.exports.getUsersParChamp = (champ, critere, callback, limit) => {
+    let filtre = {[champ]: RegExp(critere, "i")};
+    console.log(filtre);
+    Users.find(filtre, callback).limit(limit);
+}
+
+// fonction pour ajouter un usager dans la collection
+// reçoit en paramètre l'usager à ajouter et une fonction callback
+module.exports.ajoutUser = (usager, callback) => {
+    // validation du format de l'usager reçu en paramètre
+    // ajout d'un _id unique obligatoire...
+    usager._id = new mongoose.Types.ObjectId();
+    Users.create(usager, callback);
+}
+
+// fonction pour modifier un usager dans la collection
+// reçoit en paramètres:
+// - l'identifiant de l'usager à modifier
+// - l'usager à modifier (les nouvelles valeurs) et 
+// - une fonction callback
+module.exports.modifierUser = (_id, usager, callback) => {
+    // validation du format de l'usager reçu en paramètre (plus tard)
+    let filtre = {"_id": _id};
+    let options = {};
+    let nouveauUser = {
+        "nom": usager.nom,
+        "pwd": usager.pwd,
+        "login": usager.login,
+        "date": usager.date
+    };
+    Users.findOneAndUpdate(filtre, nouveauUser, options, callback);
+};
+// fonction pour supprimer un usager de la collection
+// reçoit en paramètres:
+// - l'identifiant de l'usager à supprimer 
+// - une fonction callback
+module.exports.supprimerUser = (_id, callback) => {
+    let filtre = {"_id": _id};
+    Users.deleteOne(filtre, callback);
+}
--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,50 @@ router.get('/', (requete, reponse)=>{
     }, 25);
 });
 
+router.get('/_id/:_id', (requete, reponse)=>{
+    let _id = requete.params._id;
+    Users.getUsersParId(_id, (err, users)=>{
+        if (err) throw err;
+        reponse.json(users);
+    }, 25);
+});
+
+router.get('/nom/:nom', (requete, reponse)=>{
+    let nom = requete.params.nom;
+    Users.getUsersParChamp("nom", nom, (err, users)=>{
+        if (err) throw err;
+        reponse.json(users);
+    }, 25);
+});
+
+router.get('/login/:login', (requete, reponse)=>{
+    let login = requete.params.login;
+    Users.getUsersParChamp("login", login, (err, users)=>{
+        if (err) throw err;
+        reponse.json(users);
+    }, 25);
+});
+
+router.post('/', (requete, reponse)=>{
+    let usager = requete.body;
+    Users.ajoutUser(usager, (err, msgRetour)=>{
+        if (err) throw err;
+        reponse.json(msgRetour);
+    });
+});
+router.put('/:_id', (requete, reponse)=>{
+    let _id = requete.params._id;
+    let nouveauUser = requete.body;
+    Users.modifierUser(_id, nouveauUser, (err, resultat)=>{
+        if (err) throw err;
+        reponse.json(resultat);
+    });
+});
+router.delete('/:_id', (requete, reponse)=>{
+    let _id = requete.params._id;
+    Users.supprimerUser(_id, (err, resultat)=>{
+        if (err) throw err;
+        reponse.json(resultat);
+    });
+});
 module.exports = router;
